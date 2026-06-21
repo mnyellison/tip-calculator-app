c2c19f5616bb9bbd0071add648d634440df35749
mnyellison <nmatheus.moura@hotmail.com>
2026-06-20 21:25:50 -0300

    feat: complete tip calculator logic and improve UX constraints

--- js/script.js
+++ js/script.js
@@ -11,8 +11,24 @@ let bill = 0;
 let people = 0;
 let tipPercentage = 0;
 
+function toggleResetButton() {
+  if (
+    inputBill.value !== "" ||
+    inputPeople.value !== "" ||
+    inputCustomTip.value !== "" ||
+    tipPercentage > 0
+  ) {
+    btnReset.disabled = false;
+  } else {
+    btnReset.disabled = true;
+  }
+}
+
 function handleBillInput() {
   bill = parseFloat(inputBill.value);
+
+  toggleResetButton();
+  calculateTip();
 }
 
 function handlePeopleInput() {
@@ -26,6 +42,9 @@ function handlePeopleInput() {
     inputPeople.parentElement.classList.remove("error-message");
     peopleError.classList.remove("error-message");
   }
+
+  toggleResetButton();
+  calculateTip();
 }
 
 buttons.forEach((button) => {
@@ -38,6 +57,9 @@ buttons.forEach((button) => {
     tipPercentage = parseFloat(button.dataset.percentage);
 
     inputCustomTip.value = "";
+
+    toggleResetButton();
+    calculateTip();
   });
 });
 
@@ -50,7 +72,52 @@ function handleCustomTipInput() {
   if (isNaN(tipPercentage)) {
     tipPercentage = 0;
   }
+
+  toggleResetButton();
+  calculateTip();
 }
+
+function calculateTip() {
+  if (isNaN(bill) || bill <= 0 || isNaN(people) || people <= 0) {
+    tipAmountResult.innerText = "0.00";
+    totalAmountResult.innerText = "0.00";
+    return;
+  }
+
+  const totalTip = bill * (tipPercentage / 100);
+
+  // calcula quanto de gorjeta cada pessoa vai pagar
+  const tipPerPerson = totalTip / people;
+
+  // calcula o total por pessoa (conta dividida + gorjeta dividida)
+  const totalPerPerson = bill / people + tipPerPerson;
+
+  tipAmountResult.innerText = tipPerPerson.toFixed(2);
+  totalAmountResult.innerText = totalPerPerson.toFixed(2);
+}
+
+btnReset.addEventListener("click", () => {
+  bill = 0;
+  people = 0;
+  tipPercentage = 0;
+
+  inputBill.value = "";
+  inputPeople.value = "";
+  inputCustomTip.value = "";
+
+  tipAmountResult.innerText = "0.00";
+  totalAmountResult.innerText = "0.00";
+
+  // remove o erro visual se caso estiver ativo
+  inputPeople.parentElement.classList.remove("error-message");
+  peopleError.classList.remove("error-message");
+
+  // remove o botão selecionado
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+
+  // Na última linha do clique do btnReset:
+  btnReset.disabled = true;
+});
 
 inputBill.addEventListener("input", handleBillInput);
 inputPeople.addEventListener("input", handlePeopleInput);
